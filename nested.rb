836318dcf49e2f46ae2db6a4473e90ceb0955468
Stacy Johns <stacykjohns@icloud.com>
2019-07-24 21:18:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,10 +83,8 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = {
-   :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-}
+		 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+		 return programmer_hash
 end
 
 def changing_alan
@@ -107,7 +105,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
+		 programmer_hash[:alan_kay][:known_for] = "GUI"
+		 		 return programmer_hash
 
 end
 
@@ -127,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 		 return programmer_hash
 end
